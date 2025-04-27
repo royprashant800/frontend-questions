6594a9f85ed7d8ff1e963dc7a1fb67aded202df0
Prashant <royprashant800@gmail.com>
2025-04-28 01:58:31 +0530

    some frontend questions

---
+++ Tab Components/index.js
@@ -0,0 +1,21 @@
+const tabButtons = document.querySelectorAll('.tabs__pills .btn');
+const tabContents = document.querySelectorAll('.tabs__panels > div');
+
+if (tabButtons && tabContents) {
+  tabButtons.forEach((tabBtn) => {
+    tabBtn.addEventListener('click', () => {
+    const tabId = tabBtn.getAttribute('data-id');
+
+    tabButtons.forEach((btn) => btn.classList.remove('active'));
+    tabBtn.classList.add('active');
+
+    tabContents.forEach((content) => {
+        content.classList.remove('active');
+
+        if (content.id === tabId) {
+            content.classList.add('active');
+        }
+    });
+    });
+  });
+}
