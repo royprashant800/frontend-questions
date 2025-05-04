741cd8c817dd0530263cd2b7e380db65518a581a
Prashant <royprashant800@gmail.com>
2025-05-05 02:43:14 +0530

    progress-bar

---
+++ Progress-bar/index.js
@@ -0,0 +1,15 @@
+const currentProgressBar = document.getElementById('current-progress');
+let initialProgress = 1;
+
+currentProgressBar.style.width = `${initialProgress}%`;
+
+window.addEventListener("load", () => {
+    const intv = setInterval(() => {
+        initialProgress = initialProgress + 1;
+        currentProgressBar.style.width = `${initialProgress}%`;
+
+        if(initialProgress >= 100) {
+            clearInterval(intv);
+        }
+    }, 100)
+});
