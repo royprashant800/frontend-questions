9ed22c80ea82716793e7a3e0d98b025185e1b7d1
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2025-06-28 14:36:29 +0000

    just s check

--- ReactJS/Test/src/App.jsx
+++ ReactJS/Test/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   const countHandler = () => {
     setCount(count + 1);
-  }
+  };
 
   return (
     <>
       <div>
-        <button onClick={countHandler}>Click Me</button>
+        <button onClick={countHandler}>Click</button>
         <div>{count}</div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
