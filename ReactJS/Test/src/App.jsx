e9cb357f5fee99e1f593eb48c75b8803e09d9889
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2025-06-29 07:20:34 +0000

    practice

--- ReactJS/Test/src/App.jsx
+++ ReactJS/Test/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const countHandler = () => {
+  const handleCount = () => {
     setCount(count + 1);
   };
-
   return (
     <>
       <div>
-        <button onClick={countHandler}>Click</button>
-        <div>{count}</div>
+        {count}
+        <button onClick={handleCount}>Counter</button>
       </div>
     </>
   );
--- VanillaJS/Stopwatch/index.js
+++ VanillaJS/Stopwatch/index.js
@@ -1,40 +1,40 @@
-let form = document.getElementById('container');
-let startbtn = document.getElementById('start-btn');
-let resetbtn = document.getElementById('reset-btn');
-let timer = document.getElementById('timer');
-let hour = document.getElementById('hour');
-let minute = document.getElementById('minute');
-let secs = document.getElementById('secs');
+let form = document.getElementById("container");
+let startbtn = document.getElementById("start-btn");
+let resetbtn = document.getElementById("reset-btn");
+let timer = document.getElementById("timer");
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let secs = document.getElementById("secs");
 
-let sec = 3590;
+let sec = 0;
 let inte = null;
 
 const resetStopwatch = () => {
-    hour.innerText = "00";
-    minute.innerText = "00";
-    secs.innerText = "00";
-}
+  hour.innerText = "00";
+  minute.innerText = "00";
+  secs.innerText = "00";
+};
 
-window.addEventListener('load', () => resetStopwatch());
+window.addEventListener("load", () => resetStopwatch());
 
-startbtn.addEventListener('click', () => {
-    startbtn.style.pointerEvents = 'none';
-    inte = setInterval(() => {
-        sec++;
-        // console.log(sec);
-        let minDisplay = Math.floor((sec%3600) / 60);
-        let hourDisplay = Math.floor(sec / 3600);
-        let secDisplay = Math.floor(sec % 60);
-        
-        hour.textContent = hourDisplay.toString().padStart(2, "0");
-        minute.textContent = minDisplay.toString().padStart(2, "0");
-        secs.textContent = secDisplay.toString().padStart(2, "0");
-    }, 1000);
+startbtn.addEventListener("click", () => {
+  startbtn.style.pointerEvents = "none";
+  inte = setInterval(() => {
+    sec++;
+    // console.log(sec);
+    let minDisplay = Math.floor((sec % 3600) / 60);
+    let hourDisplay = Math.floor(sec / 3600);
+    let secDisplay = Math.floor(sec % 60);
 
-    resetbtn.addEventListener('click', () => {
-        startbtn.style.pointerEvents = 'auto';
-        clearInterval(inte);
-        sec = 0;
-        resetStopwatch();
-    })
-})
+    hour.textContent = hourDisplay.toString().padStart(2, "0");
+    minute.textContent = minDisplay.toString().padStart(2, "0");
+    secs.textContent = secDisplay.toString().padStart(2, "0");
+  }, 1000);
+
+  resetbtn.addEventListener("click", () => {
+    startbtn.style.pointerEvents = "auto";
+    clearInterval(inte);
+    sec = 0;
+    resetStopwatch();
+  });
+});
